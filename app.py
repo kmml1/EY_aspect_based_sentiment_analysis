77f0f7d43e5873b9ee048d8969b2a23b0f58d353
Kamil <kowieskik@gmail.com>
2021-01-25 22:41:23 +0100

    test

--- app.py
+++ app.py
@@ -1,171 +1,37 @@
 from flask import Flask
 from flask_cors import CORS
+from flask import jsonify
 
 app = Flask(__name__)
 CORS(app)
 
+hashtags = ["kwarantanna", "vege", "IgaŚwiatek", "hot16challange", "fitness", "krolowezycia", "kryzys", "ikea", "łódź",
+            "haloween", "kawa", "radom", "karmieniepiersia", "pomidorowa", "COVID19", "nvidia", "poniedziałek",
+            "biedronka"]
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+json_out = {
+    "hash_tag": "global",
+    "date": 20210124,
+    "positive": 24800354,
+    "neutral": 224398833,
+    "negative": 224398833,
+}
 
 
-@app.route('/hello')
+@app.route('/')
 def say_hello_world():
-    return {'result': "W końcu kurwa"}
+    return jsonify(json_out)
 
 
 @app.route("/global")
 def hello():
-    return "global hello"
-
-
-h1 = "/kwarantanna"
-
-
-@app.route(h1)
-def hello():
-    return "Hello, World!", h1
-
-
-h2 = "/vege"
-
-
-@app.route(h2)
-def hello():
-    return "Hello, World!", h2
-
-
-h3 = "/IgaŚwiatek"
-
-
-@app.route(h3)
-def hello():
-    return "Hello, World!", h3
-
-
-h4 = "hot16challange"
-
-
-@app.route(h4)
-def hello():
-    return "Hello, World!", h4
-
-
-h5 = "fitness"
-
-
-@app.route(h5)
-def hello():
-    return "Hello, World!", h5
-
-
-h6 = "krolowezycia"
-
-
-@app.route(h6)
-def hello():
-    return "Hello, World!", h6
-
-
-h7 = "kryzys"
-
-
-@app.route(h7)
-def hello():
-    return "Hello, World!", h7
-
-
-h8 = "ikea"
-
-
-@app.route(h8)
-def hello():
-    return "Hello, World!", h8
-
-
-h9 = "łódź"
-
-
-@app.route(h9)
-def hello():
-    return "Hello, World!", h9
-
-
-h10 = "haloween"
-
-
-@app.route(h10)
-def hello():
-    return "Hello, World!", h10
-
-
-h11 = "kawa"
-
-
-@app.route(h11)
-def hello():
-    return "Hello, World!", h11
-
-
-h12 = "radom"
-
-
-@app.route(h12)
-def hello():
-    return "Hello, World!", h12
-
-
-h13 = "karmieniepiersia"
-
-
-@app.route(h13)
-def hello():
-    return "Hello, World!", h13
-
-
-h14 = "pomidorowa"
-
-
-@app.route(h14)
-def hello():
-    return "Hello, World!", h14
-
-
-h15 = "COVID19"
-
-
-@app.route(h15)
-def hello():
-    return "Hello, World!", h15
-
-
-h16 = "nvidia"
-
-
-@app.route(h16)
-def hello():
-    return "Hello, World!", h16
-
-
-h17 = "poniedziałek"
-
-
-@app.route(h17)
-def hello():
-    return "Hello, World!", h17
-
-
-h18 = "biedronka"
-
-
-@app.route(h18)
-def hello():
-    return "Hello, World!", h18
-
-# {
-#   "date": 20210124,
-#   "positive": 24800354,
-#   "neutral": 224398833,
-#   "negative": 224398833,
-# },
+    json_out["hash_tag"] = "global"
+    return jsonify(json_out)
+
+
+@app.route('/<id>')
+def landing_page(id):
+    if id not in hashtags:
+        return {"hash_tag": "invalid"}
+    json_out["hash_tag"] = id
+    return jsonify(json_out)
