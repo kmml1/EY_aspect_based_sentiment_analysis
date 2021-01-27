fa9224bd881e9b14038e939bc26d5ff9455d9e56
Bartłomiej Łukasik <01153066@pw.edu.pl>
2021-01-27 04:03:44 +0100

    Update azureDBconnections.py

--- twitter_program/azureDBconnections.py
+++ twitter_program/azureDBconnections.py
@@ -6,15 +6,14 @@ from twitter_program import zamianaZnakow as zz
 
 def select(table):
     table = zz.odkoduj(table)
-    select = "SELECT * FROM " + table
+    select = "SELECT * FROM "+ table
     server = credentials.server
     database = credentials.database
     username = credentials.username
-    password = credentials.password
+    password = credentials.password 
     driver = credentials.driver
 
-    with pyodbc.connect(
-            'DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password) as conn:
+    with pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
         with conn.cursor() as cursor:
             cursor.execute(select)
             lista = []
@@ -24,115 +23,61 @@ def select(table):
                 row = cursor.fetchone()
             return lista
 
+def query(conn,query):
+    
+    with conn.cursor() as cursor:
+        try:
+            cursor = conn.cursor()
+            cursor.execute(query)
+        except:  
+            print(query)
+            
+def create(conn,table):
+    table = zz.odkoduj(table)
+    string = "if not exists (select * from sysobjects where name='" + table +"' and xtype='U') CREATE TABLE " + table +" (content ntext, date ntext,sentiment int);"
+    query(conn,string)
 
-def query(query):
+def close(conn):
+    conn.close()
+
+def connect():
     server = credentials.server
     database = credentials.database
     username = credentials.username
-    password = credentials.password
+    password = credentials.password 
     driver = credentials.driver
-
-    with pyodbc.connect(
-            'DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password) as conn:
-        with conn.cursor() as cursor:
-            try:
-                cursor.execute(query)
-            except:
-                print(query)
-
-
-def create(table):
+    return pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+    
+def insert(conn,table,content,date,sentiment):
     table = zz.odkoduj(table)
-    string = "if not exists (select * from sysobjects where name='" + table + "' and xtype='U') CREATE TABLE " + table + " (content ntext, date ntext,sentiment int);"
-    query(string)
+    content.replace("'","")
+    string = "INSERT INTO [" + table + "] (content, date, sentiment) VALUES ('"+ content + "','"+ date + "','"+ str(sentiment) + "');"
+    query(conn,string)
 
-
-def insert(table, content, date, sentiment):
+def drop(conn,table):
     table = zz.odkoduj(table)
-    string = "INSERT INTO [" + table + "] (content, date, sentiment) VALUES ('" + content + "','" + date + "','" + str(
-        sentiment) + "');"
-    query(string)
+    string = "if exists (select * from sysobjects where name='" + table +"' and xtype='U') DROP TABLE " + table +";"
+    query(conn,string)
 
-
-def drop(table):
+def deleteFromController(conn,table,name):
     table = zz.odkoduj(table)
-    string = "if exists (select * from sysobjects where name='" + table + "' and xtype='U') DROP TABLE " + table + ";"
-    query(string)
+    string = "DELETE FROM " + table +" WHERE name = '" + name + "';"
+    query(conn,string)
 
+def insertIntoController(conn,name):
+    string = "INSERT INTO CONTROLLER (name) VALUES ('"+ name + "');"
+    query(conn,string)
 
-def deleteFromController(table, name):
-    table = zz.odkoduj(table)
-    string = "DELETE FROM " + table + " WHERE name = '" + name + "';"
-    query(string)
-
-
-def insertIntoController(name):
-    string = "INSERT INTO CONTROLLER (name) VALUES ('" + name + "');"
-    query(string)
-
-
-def createController():
+def createController(conn):
     string = "if not exists (select * from sysobjects where name='CONTROLLER' and xtype='U') CREATE TABLE CONTROLLER (name ntext);"
-    query(string)
+    query(conn,string)
 
-
-def resetAll():
-    hashtags = ["kwarantanna", "vege", "IgaŚwiatek", "hot16challange", "fitness", "krolowezycia", "kryzys", "ikea",
-                "łódź", "haloween", "kawa", "radom", "karmieniepiersia", "pomidorowa", "COVID19", "nvidia",
-                "poniedziałek", "biedronka"]
+def resetAll(conn):
+    hashtags = ["kwarantanna", "vege","IgaŚwiatek","hot16challange","fitness","krolowezycia","kryzys","ikea","łódź","haloween","kawa","radom","karmieniepiersia","pomidorowa","COVID19","nvidia","poniedziałek","biedronka"]
     for hashtag in hashtags:
-        drop(hashtag)
-        create(hashtag)
+        drop(conn,hashtag)
+        create(conn,hashtag)
 
-
-def resetHashtag(hashtag):
-    drop(hashtag)
-    create(hashtag)
-
-
-def test():
-    """create('TEST')
-    insert('TEST','twit1','1.11.2021',-1)
-    insert('TEST','twit2','2.11.2021',1)
-    insert('TEST','twit3','3.11.2021',1)
-    insert('TEST','twit4','4.11.2021',-1)
-    insert('TEST','twit5','5.11.2021',1)
-    insert('TEST','twit6','6.11.2021',1)
-    insert('TEST','twit7','7.11.2021',0)
-    insert('TEST','twit8','8.11.2021',0)
-    insert('TEST','twit9','9.11.2021',0)
-    insert('TEST','twit10','10.11.2021',1)
-    insert('TEST','twit11','11.11.2021',-1)
-    create('TEST2')
-    insert('TEST2','twit1','1.10.2021',-1)
-    insert('TEST2','twit2','2.10.2021',0)
-    insert('TEST2','twit3','3.10.2021',1)
-    insert('TEST2','twit4','4.10.2021',0)
-    insert('TEST2','twit5','5.10.2021',1)
-    insert('TEST2','twit6','6.10.2021',1)
-    insert('TEST2','twit7','7.10.2021',0)
-    insert('TEST2','twit8','8.10.2021',1)
-    insert('TEST2','twit9','9.10.2021',0)
-    insert('TEST2','twit10','10.10.2021',1)
-    insert('TEST2','twit11','11.10.2021',1)
-    createController()
-    insertIntoController('TEST1')
-    insertIntoController('TEST2')
-    table = select("CONTROLLER")
-    print(table.pop(0)[0])
-    print(table.pop(0)[0])"""
-
-    # create('testowanie')
-    # create('TEST3')
-    # insert('TEST3','test','11.10.2021',1)
-    # table = db.select("testowanie")
-    # rekord = table.pop(0)
-    # print(rekord[0]) # tekst
-    # print(rekord[1]) # data
-    # print(rekord[2]) # sentyment
-    hashtags = ["kwarantanna", "vege", "IgaŚwiatek", "hot16challange", "fitness", "krolowezycia", "kryzys", "ikea",
-                "łódź", "haloween", "kawa", "radom", "karmieniepiersia", "pomidorowa", "COVID19", "nvidia",
-                "poniedziałek", "biedronka"]
-    for hashtag in hashtags:
-        table = select(hashtag)
-        print(hashtag + " " + str(len(table)))
+def resetHashtag(conn,hashtag):
+    drop(conn,hashtag)
+    create(conn,hashtag)
