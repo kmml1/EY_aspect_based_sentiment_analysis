f474425d2eb9e06d04e4e8ab7412a6e502a4ef7c
Bartłomiej Łukasik <01153066@pw.edu.pl>
2021-01-27 04:02:51 +0100

    Update script.py

--- twitter_program/script.py
+++ twitter_program/script.py
@@ -6,64 +6,64 @@ from twitter_program import sentimentModel
 from twitter_program import azureDBconnections
 
 
-def min(s, low):
+def min(s,low):
     if (low < s):
-        return low
-    else:
+        return low         
+    else: 
         return s
 
-
 def refreshDB(i):
-    CONSUMER_KEY = credentials.CONSUMER_KEY
-    CONSUMER_SECRET = credentials.CONSUMER_SECRET
+    CONSUMER_KEY= credentials.CONSUMER_KEY
+    CONSUMER_SECRET= credentials.CONSUMER_SECRET
     python_tweets = Twython(CONSUMER_KEY, CONSUMER_SECRET)
 
-    hashtags = ["kwarantanna", "vege", "IgaŚwiatek", "hot16challange", "fitness", "krolowezycia", "kryzys", "ikea",
-                "łódź", "haloween", "kawa", "radom", "karmieniepiersia", "pomidorowa", "COVID19", "nvidia",
-                "poniedziałek", "biedronka"]
+    hashtags = ["kwarantanna", "vege", "IgaŚwiatek","hot16challange","fitness","krolowezycia","kryzys","ikea","łódź","haloween","kawa","radom","karmieniepiersia","pomidorowa","COVID19","nvidia","poniedziałek","biedronka"]
     my_date = datetime.date.today()
     my_date = my_date - datetime.timedelta(days=i)
     day = str(calendar.day_name[my_date.weekday()])[0:3]
+    conn = azureDBconnections.connect()
+    conn2 = sentimentModel.connect()
     for hashtag in hashtags:
-        azureDBconnections.create(hashtag)
+        azureDBconnections.create(conn,hashtag)
     for hashtag in hashtags:
 
         lowest = None
         # Create our query
-        query = {'q': '#' + hashtag,
-                 'count': 10,
-                 'lang': 'pl',
-                 'tweet_mode': 'extended',
-                 }
+        query = {'q': '#'+hashtag,
+                'count': 10,
+                'lang': 'pl',
+                'tweet_mode': 'extended',
+                }
         # Search tweets
-
+        
         lowest = None
         lowest_tmp = 1
-        while lowest != lowest_tmp:
+        while (lowest != lowest_tmp):
             lowest_tmp = lowest
-            query = {'q': '#' + hashtag,
-                     'lang': 'pl',
-                     'tweet_mode': 'extended',
-                     'max_id': lowest,
-                     }
+            query = {'q': '#'+ hashtag,
+                    'lang': 'pl',
+                    'tweet_mode': 'extended',
+                    'max_id': lowest,
+            }
             for status in python_tweets.search(**query)['statuses']:
-
-                if str(status['created_at'])[0:3] == day and status['id'] != lowest:
+                
+                if (str(status['created_at'])[0:3] == day and status['id']!=lowest):
                     if 'retweeted_status' in status.keys():
-                        s = status['full_text'].replace(",", ";")
-                    else:
-                        s = status['full_text'].replace(",", ";")
-                        s = s.replace("'", "")
-                        tmp = sentimentModel.sentiment(s)
+                        s = status['full_text'].replace("'","")
+                        s = status['full_text'].replace("'","")
+
+                        tmp = sentimentModel.sentiment(conn2,s)
                         sentiment = 0
-                        if (tmp < 0.5):
+                        if (tmp < 0.5 ):
                             sentiment = -1
                         if (tmp > 0.5):
                             sentiment = 1
-
-                        azureDBconnections.insert(hashtag, s, status['created_at'], sentiment)
-
+                        
+                        azureDBconnections.insert(conn,hashtag,s,status['created_at'],sentiment)
+                    
                 if lowest is None:
-                    lowest = status['id']
+                    lowest =  status['id']
                 else:
-                    lowest = min(status['id'], lowest)
+                    lowest = min(status['id'],lowest)
+    azureDBconnections.close(conn)
+    sentimentModel.close(conn2)
